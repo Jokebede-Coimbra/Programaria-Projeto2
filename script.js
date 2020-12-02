d0d1db8d63bb3539e6e35d88c406b3958ccea933
Coimbra <adsjkbd@gmail.com>
2020-12-02 19:13:15 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function validaFormulario() {
+
+    let email = formEmail.email.value;
+
+    if (email == "") {
+
+        alert("Por favor, preencha o campo do email.");
+
+    
+    } else {
+
+        alert("Tudo certo! Você receberá as novidades por email.")
+    }
+}
